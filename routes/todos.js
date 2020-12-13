263cce9d15344a6edcf0f48a5d4e886d50ba6ea1
Julian <vilaj46@gmail.com>
2020-12-13 16:27:58 -0500

    Testing

--- routes/todos.js
+++ routes/todos.js
@@ -11,7 +11,7 @@ router.post("/", (req, res) => {
   const { userID } = req.params;
   const newItem = req.body;
 
-  console.log(newItem);
+  console.log(req.headers);
 
   User.findById(userID, async (err, doc) => {
     if (err) return res.sendStatus(500); // Not sure, something went wrong. Could not find user.
