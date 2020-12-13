0df3b2939667439246e2f437453786590bc241b5
Julian <vilaj46@gmail.com>
2020-12-13 16:43:22 -0500

    Took out cookies for heroku.

--- middleware/validateAuthentication.js
+++ middleware/validateAuthentication.js
@@ -8,30 +8,31 @@ router.use((req, res, next) => {
   const token = authorization.split(" ")[1];
   const decodedToken = jwt_decode(token);
 
-  const { session_id } = req.cookies;
-  const { authorization } = req.headers;
+  // const { session_id } = req.cookies;
+  // const { authorization } = req.headers;
 
-  if (
-    session_id === undefined &&
-    (uthorization.length === 0 || authorization === undefined)
-  ) {
-    return res.sendStatus(403);
-  }
+  // if (
+  //   session_id === undefined &&
+  //   (uthorization.length === 0 || authorization === undefined)
+  // ) {
+  //   return res.sendStatus(403);
+  // }
 
-  let decodedCookie;
+  // let decodedCookie;
 
   // const decodedCookie = jwt_decode(session_id);
 
-  if (session_id) {
-    decodedCookie = jwt_decode(session_id);
-  } else if (authorization) {
-    console.log(authorization.split(" ")[1]);
-    decodedCookie = jwt_decode(authorization.split(" ")[1]);
-  }
+  // if (session_id) {
+  //   decodedCookie = jwt_decode(session_id);
+  // } else if (authorization) {
+  //   console.log(authorization.split(" ")[1]);
+  //   decodedCookie = jwt_decode(authorization.split(" ")[1]);
+  // }
 
   if (
-    Date.now() >= decodedToken.exp * 1000 ||
-    Date.now() >= decodedCookie * 1000
+    Date.now() >=
+    decodedToken.exp * 1000
+    // || Date.now() >= decodedCookie * 1000
   ) {
     return res.sendStatus(401);
   } else {
